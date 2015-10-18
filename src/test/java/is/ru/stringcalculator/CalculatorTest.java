1cc62bea43b966cf26bdd04587a446365d5ce524
Stinni <kristinnf13@ru.is>
2015-10-18 00:20:50 +0000

    added test for and implemented return of two numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -18,4 +18,9 @@ public class CalculatorTest {
 	public void testAddWithOneNumber() {
 		assertEquals(5, Calculator.add("5"));
 	}
+
+	@Test
+	public void testAddWithTwoNumbers() {
+		assertEquals(20, Calculator.add("12,8"));
+	}
 }
